c75fd3fb35ce450310639c546860e083b9e5259f
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-29 18:00:12 +0900

    [add]Users一覧画面設置

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update, :index]
 end
