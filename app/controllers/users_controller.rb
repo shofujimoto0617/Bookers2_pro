6d3a6f0c3cade6436ce979327a4778ea821be07d
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-29 16:37:32 +0900

    [add]Topページ設置

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-  	params.require(:user).permit(:name, :introduction, :user_image)
+  	params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
