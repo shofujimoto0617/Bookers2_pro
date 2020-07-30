c8af3e38c6f1bff33972c0498e703f66aca4ec22
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-30 21:19:36 +0900

    [add]Bookモデル,

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
 
   def index
+  	@books = Book.all
   end
 
   def new
@@ -8,6 +9,10 @@ class BooksController < ApplicationController
   end
 
   def create
+  	@book = Book.new(book_params)
+  	@book.user_id = current_user.id
+  	@book.save
+  	redirect_to books_path
   end
 
   def show
@@ -17,6 +22,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
   end
 
 end
