01ef3be74c55b6300788d6f66f228a384ea46ff5
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-30 18:22:20 +0900

    [add]Bookモデル,controller生成

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show, :edit, :update, :index]
+  resources :books
 end
