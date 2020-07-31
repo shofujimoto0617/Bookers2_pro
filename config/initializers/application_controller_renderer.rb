6bc2dc88423bb7ff1722f1d0b6d64fe0383f2376
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-31 14:13:14 +0900

    [Update]Link設定

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '42d84ec5890526259be0391798eb86adf92669def97c77a3276e3b1d399111a30466348318764afe94cf70fdfdfc94f2505306fe472aedcb55dbe2a25f389c00'
